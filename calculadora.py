540c5260f38f4acce79ac6111c71a034e9bd6ae1
Aloísio Sousa <aloisio.ssousa@gmail.com>
2025-08-26 22:25:51 -0300

    Feat: Add calculator functionality

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+print("Calculadora")
+entrada_numero1 = input("Digite o primeiro numero: ")
+entrada_numero2 = input("Digite o segundo numero: ")
+
+numero1 = int(entrada_numero1)
+numero2 = int(entrada_numero2)
+
+resultado = numero1 + numero2
+
+print(f"A soma de número {numero1} + {numero2} é o {resultado}")
