a53956b67b52875a60d90df2102692db02afaf4f
Aloísio Sousa <aloisio.ssousa@gmail.com>
2025-08-27 21:53:03 -0300

    Feat: Adiciona tratamento de erro para entrada de dados invalidos

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,43 @@
-print("Calculadora")
+
+def somar (num1, num2):
+    resultado_soma = num1 + num2
+    return resultado_soma
+
+def multiplicar (num1, num2):
+    resultado_multiplicacao = num1 * num2
+    return resultado_multiplicacao
+
+def subtrair (num1, num2):
+    resultado_subtracao = num1 - num2
+    return resultado_subtracao
+
+def dividir (num1, num2):
+    if num2 == 0:
+        return "Erro: Divisão por Zero não é permitido"
+    else: 
+        resultado_divisao = num1 / num2
+        return resultado_divisao
+
+
+if __name__ == "__main__":
+    print("Calculadora")
+
+
 entrada_numero1 = input("Digite o primeiro numero: ")
 entrada_numero2 = input("Digite o segundo numero: ")
 
-numero1 = int(entrada_numero1)
-numero2 = int(entrada_numero2)
+try:
+    num1_int = int(entrada_numero1)
+    num2_int = int(entrada_numero2)
 
-resultado = numero1 * numero2
+    soma = somar(num1_int, num2_int)
+    multiplicacao = multiplicar(num1_int, num2_int)
+    subtracao = subtrair(num1_int, num2_int)
+    divisao = dividir(num1_int, num2_int)
 
-print(f"A Multiplicação de número {numero1} + {numero2} é o {resultado}")
+    print(f"A somar é {soma}")
+    print(f"A multiplicacao é {multiplicacao}")
+    print(f"A subtracao é {subtracao}")
+    print(f"A divisao é {divisao}")
+except:
+    print("Erro: Por favor, digite apenas números inteiro")
